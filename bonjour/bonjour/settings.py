293193665ec3dd462af225ef2c02e0f11db86197
Pavel Shimko <pavel.shimko.nur@gmail.com>
2022-10-24 16:17:19 +0300

    put right pipeline into place

--- bonjour/bonjour/settings.py
+++ bonjour/bonjour/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "bonjour.pipelines.BonjourPipeline": 300,
+    "bonjour.pipelines.BonjourCSVPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
